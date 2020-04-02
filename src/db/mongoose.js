7aa2917142b1832dd84e71ee58ffa98009109a34
beingAkash <beingakki26@gmail.com>
2020-04-03 02:27:37 +0530

    Npm Validtor

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose')
+const validator=require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
 
@@ -10,16 +11,51 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
 
 const Tasks =mongoose.model('tasks',{
     Description:{
-        type:String
+        type:String,
+        required: true,
+        trim:true
+    },
+    mail:{
+        type: String,
+        required:true,
+        trim:true,
+        lowercase:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email Invalid')
+            }
+        }
+
     },
     Completed:{
-        type:Boolean
+        type:Number,
+        validate(value){
+            if(value<0){
+               throw new Error('Age must be +ve no')
+            }
+        }
+    },
+    Password:{
+        type:String,
+        trim:true,
+        minlength:7,
+        maxlength:9,
+        validate(value){
+            if(value.toLowerCase().includes('password')){
+
+                throw new Error('Password cannot contain "password"')
+
+            }
+        }        
+
     }
 })
 
 const metoo=new Tasks({
-    Description: 'Learn MOngoose',
-    Completed:false
+    Description: 'Dummey',    
+    mail:' kite@alphao.com',
+    Password:'Akash38#',
+    Completed:6
 
 })
 
@@ -28,7 +64,7 @@ metoo.save().then(()=>{
 
     console.log(metoo)
 }).catch((e)=>{
-    console.log('Error!!')
+    console.log('Error!!',e)
 })
 
 // const User=mongoose.model('User',{
