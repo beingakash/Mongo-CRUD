f9b71e7060f7f01826d7b3871c8b3471d3e53a32
beingAkash <beingakki26@gmail.com>
2020-04-08 04:16:45 +0530

    Use of BCrypt.js

--- src/index.js
+++ src/index.js
@@ -7,3 +7,17 @@ app.listen(port, () => {
 
 
 
+const bcrypt=require('bcryptjs')
+
+const myFun=async()=>{
+    const password='RED1234'
+    const hashed= await bcrypt.hash(password,8)
+
+    console.log(password)
+    console.log(hashed)
+
+    const isMatch=await bcrypt.compare('RED1234',hashed)
+    console.log(isMatch)
+}
+
+myFun()
